c71c1bc250f0c3bed5ec8463ca3b6ad0c88471ac
Plutonicx <stojanovich.stevan@live.com>
2016-02-20 15:42:00 +1100

    Added unit test

---
+++ app/src/androidTest/java/connectthedots/com/connectthedots/DrawerTest.java
@@ -0,0 +1,90 @@
+package connectthedots.com.connectthedots;
+
+/**
+ * Created by Stevan on 2/20/2016.
+ */
+
+/*
+
+
+import junit.Test;
+import java.util.ArrayList;
+
+import dalvik.annotation.TestTargetClass;
+
+
+public class DrawerTest {
+
+    @Test
+    public void basicTest(){
+
+        assert(1 == 2);
+    }
+
+*/
+/*
+    @Test
+    public void drawingLimitDefaultConstructor(){
+
+        DrawingLimits limits = new DrawingLimits(1);
+
+        assert(limits != null);
+    }
+
+    @Test
+    public void canActivateDefault(){
+
+        DrawingLimits limits = new DrawingLimits(1);
+
+        //boolean tester = limits.activateDot(160, 460);
+        boolean tester = limits.activateDot(160, 1150);
+
+        assert(tester == true);
+    }
+
+    @Test
+    public void isInLine(){
+
+        DrawingLimits limits = new DrawingLimits(1);
+
+        //boolean tester = limits.activateDot(160, 460);
+        //boolean test2 = limits.isInLine(800, 930);
+        boolean tester = limits.activateDot(160, 1150);
+        boolean test2 = limits.isInLine(300, 1100);
+
+        assert(test2 == true);
+    }
+
+    @Test
+    public void isNotInLine(){
+
+        DrawingLimits limits = new DrawingLimits(1);
+
+        //boolean tester = limits.activateDot(160, 460);
+        //boolean test2 = limits.isInLine(1000, 930);
+        boolean tester = limits.activateDot(160, 1150);
+        boolean test2 = limits.isInLine(350, 523);
+
+        assert(test2 == false);
+    }
+
+    @Test
+    public void isGoingForward(){
+        DrawingLimits limits = new DrawingLimits(1);
+
+        boolean tester = limits.activateDot(30, 160);
+        boolean test2 = limits.isGoingForward(216, 260, 220, 270);
+
+        assert(test2 == true);
+    }
+
+    @Test
+    public void getData(){
+        LevelBuilder build = new LevelBuilder();
+
+    }*//*
+
+
+}
+
+*/
---
+++ app/src/test/java/connectthedots/com/connectthedots/DrawerTest.java
@@ -0,0 +1,25 @@
+package connectthedots.com.connectthedots;
+
+/**
+ * Created by Stevan on 2/20/2016.
+ */
+
+
+import org.junit.Test;
+import java.util.ArrayList;
+
+
+public class DrawerTest {
+
+    @Test
+    public void basicTest(){
+
+        String test = "hello";
+
+        test = test + " there...";
+
+        assert(true == true);
+    }
+
+
+}
